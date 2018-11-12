51dde38b4b216139e6f7a60068afd2a6379353b0
Diego Correa | Stone <dcorrea@stone.com.br>
2018-11-12 14:52:58 -0200

    Add Batch Publish

---
+++ RabbitMQSimpleProducer/MessageSet.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQSimpleProducer
+{
+    public class MessageSet<T>
+    {
+        public MessageSet(T message, string exchange, string routingKey, bool mandatory)
+        {
+            Message = message;
+            Exchange = exchange;
+            RoutingKey = routingKey;
+            Mandatory = mandatory;
+        }
+
+        public T Message { get; set; }
+
+        //public U Destination { get; set; }
+
+        public string Exchange { get; set; }
+        public bool Mandatory { get; set; }
+        public string RoutingKey { get; set; }
+        public IBasicProperties BasicProperties { get; set; }
+
+
+        public string ToString()
+        {
+            return JsonConvert.SerializeObject(this.Message);
+        }
+    }
+
+
+
+    class Destination
+    {
+        public string Exchange { get; set; }
+        public bool Mandatory { get; set; }
+        public string RouteKey { get; set; }
+
+    }
+}
